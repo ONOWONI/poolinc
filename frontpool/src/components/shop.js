826c3a3e8f03ff54dbbffe34b8b260064a0aefdc
Onowoni <danielonowoni1@gmail.com>
2022-08-12 16:53:56 +0100

    updated store area

--- frontpool/src/components/shop.js
+++ frontpool/src/components/shop.js
@@ -1,18 +1,23 @@
-import img from '../img/pool5.jpg'
+import img from '../img/pool.jpg'
 
 export default function Shop() {
     return (
         <section className='shop' id='shop'>
-            <h3>Or shop with us</h3>
-            <div className='shop-card'>
-                <div>
-                    <img src={img} alt='shop card img' />
-                </div>
-                <div>
-                    <h4>Name</h4>
-                    <p>price</p>
-                    <span>qty</span>
-                    <p>desc</p>
+            <h3>Store</h3>
+            <div className='shop-card-container'>
+                <div className='shop-card'>
+                    <div className='flex-img'>
+                        <img src={img} alt='shop card img' />
+                    </div>
+                    <div className='card-info'>
+                        <h4 className='card-name'>Name</h4>
+                        <div className='price-qty'>
+                            <span className='card-price'>$20.00</span>
+                            <span className='card-qty'>20 Pieces</span>
+                        </div>
+                        <hr></hr>
+                        <p className='card-desc'>desc just a bunch of words to fill up space. i would have used lorem but fuck it </p>
+                    </div>
                 </div>
             </div>
         </section>
