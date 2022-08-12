85c019dcc485c26b94e6cc76ed6b1797d208f2e1
Onowoni <danielonowoni1@gmail.com>
2022-08-12 14:23:20 +0100

    updated service

--- frontpool/src/components/whatwedo.js
+++ frontpool/src/components/whatwedo.js
@@ -11,12 +11,12 @@ export default function Services() {
                 </div>
 
                 <div className='service-img right'>
-                    <img src={poster} alt='something' />
+                    <img src={poster} alt='Clean Pool' className='border-left' />
                 </div>
             </div>
-            <div className='service-card'>
+            <div className='service-card left-img'>
                 <div className='service-img'>
-                    <img src={poster} alt='something' />
+                    <img src={poster} alt='Clean Pool 2' className='border-right' />
                 </div>
                 <div className='service-text right'>
                     <h1>We will clean your pool for money</h1>
