3c9757ec9b88bb52373e3bddeb32923e04493835
Onowoni <danielonowoni1@gmail.com>
2022-08-17 12:22:01 +0100

    forgot to push yesterday

--- frontpool/src/components/reviews.js
+++ frontpool/src/components/reviews.js
@@ -1,20 +1,54 @@
+import React, { useState } from "react"
 import reviewData from "../reviewData"
+import ReviewModal from "./reviewModal"
 
 export default function Reviews() {
+    const [reviewDict, setReviewDict] = React.useState({
+        review : "The poolinc crew are so amazing at their jobs, they came to clean my pool and they did a very amazing job i had to pay double what we agreed on",
+        name : "MA NIGGA "
+    })
+    const [count, setCount] = React.useState(0)
+    const [modal, setModal] = React.useState(false)
+
+    function nextClick() {
+        if (count < reviewData.length -1) {
+            setCount(prev => prev + 1)
+        } else {
+            setCount(0)
+        }
+        setReviewDict(reviewData[count])
+        console.log(count);
+    }
+
+    function prevClick() {
+        if (count > 0 ) {
+            setCount(prev => prev - 1)
+        } else {
+            setCount(reviewData.length -1)
+        }
+        setReviewDict(reviewData[count])
+        console.log(count);
+    }
+
+
+    function closeModal(){
+        setModal(false)
+    }
     return (
         <div id="reviews" >
             <h2>Feedback from our clients</h2>
             <div className="review-container">
-                <button id="left">Prev</button>
+                <button id="left" onClick={prevClick}>Prev</button>
                 <div className="review-card">
                     <blockquote className='review-text'>
-                        The poolinc crew are so amazing at their jobs, they came to clean my pool and they did a very amazing job i had to pay double what we agreed on
+                        {reviewDict.review}
                     </blockquote>
-                    <cite className="review-owner">~ MA NIGGA ~</cite>
+                    <cite className="review-owner">~ {reviewDict.name}~</cite>
                 </div>
-                <button id="right">Next</button>
+                <button id="right" onClick={nextClick}>Next</button>
             </div>
-            <button className='header-contact' id='review-btn'>Submit your review</button>
+            <button className='header-contact' id='review-btn' onClick={() => setModal(true)}>Submit your review</button>
+            <ReviewModal modal={modal} close={() => closeModal()} />
         </div>
     )
 }
--- frontpool/src/reviewData.js
+++ frontpool/src/reviewData.js
@@ -2,41 +2,41 @@ export default [
     {
         id : 1,
         name :'jon Bellion',
-        review : 'nothing is better than this service i recieved from these niggas here today. They are the best in the market and i do not sound like AJR and they did not copy my style of music because i copied them'
+        review : '1 nothing is better than this service i recieved from these niggas here today. They are the best in the market and i do not sound like AJR and they did not copy my style of music because i copied them'
     },
     {
         id : 2,
-        name :'jon Bellion',
-        review : 'nothing is better than this service i recieved from these niggas here today. They are the best in the market and i do not sound like AJR and they did not copy my style of music because i copied them'
+        name :'AJR',
+        review : '2 That bitch jon bellion always coping us. get a life you loser'
     },
     {
         id : 3,
-        name :'jon Bellion',
-        review : 'nothing is better than this service i recieved from these niggas here today. They are the best in the market and i do not sound like AJR and they did not copy my style of music because i copied them'
+        name :'Somebody',
+        review : '3 I feel like Jon is not really sorry and that he is just lying'
     },
     {
         id : 4,
         name :'jon Bellion',
-        review : 'nothing is better than this service i recieved from these niggas here today. They are the best in the market and i do not sound like AJR and they did not copy my style of music because i copied them'
+        review : '4 nothing is better than this service i recieved from these niggas here today. They are the best in the market and i do not sound like AJR and they did not copy my style of music because i copied them'
     },
     {
         id : 5,
         name :'jon Bellion',
-        review : 'nothing is better than this service i recieved from these niggas here today. They are the best in the market and i do not sound like AJR and they did not copy my style of music because i copied them'
+        review : '5 nothing is better than this service i recieved from these niggas here today. They are the best in the market and i do not sound like AJR and they did not copy my style of music because i copied them'
     },
     {
         id : 6,
         name :'jon Bellion',
-        review : 'nothing is better than this service i recieved from these niggas here today. They are the best in the market and i do not sound like AJR and they did not copy my style of music because i copied them'
+        review : '6 nothing is better than this service i recieved from these niggas here today. They are the best in the market and i do not sound like AJR and they did not copy my style of music because i copied them'
     },
     {
         id : 7,
         name :'jon Bellion',
-        review : 'nothing is better than this service i recieved from these niggas here today. They are the best in the market and i do not sound like AJR and they did not copy my style of music because i copied them'
+        review : '7 nothing is better than this service i recieved from these niggas here today. They are the best in the market and i do not sound like AJR and they did not copy my style of music because i copied them'
     },
     {
         id : 8,
         name :'jon Bellion',
-        review : 'nothing is better than this service i recieved from these niggas here today. They are the best in the market and i do not sound like AJR and they did not copy my style of music because i copied them'
+        review : '8 Everyone is lying and nobody has ocd'
     },
 ]
