15da8afe03dcf6bcbe92a14c82d77d888eb9f875
Onowoni <danielonowoni1@gmail.com>
2022-08-18 14:33:23 +0100

    removed frontend from here

--- frontpool/src/components/footer.js
+++
@@ -1,9 +0,0 @@
-export default function Footer(){
-    return (
-        <footer>
-            <p>Disclaimer : This is not a real service, it is just a test project by the creator</p>
-            <h4>Made by Daniel Onowoni</h4>
-
-        </footer>
-    )
-}
