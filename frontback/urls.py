b98966d98ac246cd78b395a025abdd1000ffc77d
Onowoni <danielonowoni1@gmail.com>
2022-08-20 14:27:15 +0100

    another cors error5

--- frontback/urls.py
+++ frontback/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("", include('pool.urls')),
-    path("admin/", admin.site.urls),
+    path("admin", admin.site.urls),
 ]
--- pool/urls.py
+++ pool/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     re_path(r'^$', views.api_overview),
-    re_path(r'^api/products/$', views.product_list),
-    re_path(r'^api/reviews/$', views.review_list),
+    re_path(r'^api/products$', views.product_list),
+    re_path(r'^api/reviews$', views.review_list),
 ]
